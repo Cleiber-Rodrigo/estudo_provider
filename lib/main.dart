fe8f3c1a2f7fbd456b81af90f68441d19463dc70
Cleiber Rodrigo <cleiberrodrigox@hotmail.com>
2023-06-05 15:16:41 -0300

    Após implementação do Provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp(
-
-  ));
+  runApp(Provider(
+    create: (_) => Pessoa('Carlos', 30),
+    child: MyApp(),
+    ),
+  );
 }
 
 class Pessoa {
   String nome = "João";
   int idade = 30;
 
-  Pessoa();
+  Pessoa(this.nome, this.idade);
+
+  void incrementIdade(){
+   idade++;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -19,15 +25,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    var name = (context).select((Pessoa p) => p.nome);
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Estudo Provider'),
           centerTitle: true,
         ),
-        body: const Center(
+        body: Center(
           child: Text(
-            'João tem 30 anos',
+            '${name} tem 30 anos',
             style: TextStyle(
               fontSize: 35,
               fontWeight: FontWeight.bold
